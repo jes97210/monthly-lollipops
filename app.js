94bfec75f00053727553559426846891bd49e49a
Arthur Lai <alai@bcgsc.ca>
2021-02-21 21:00:24 -0800

    Added file name submission button that reads local file based on filename input.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-d3.csv('test.csv', function (data) {
-    return data
-}).then( function (data) {
-// do something with data
-    console.log(data)
-});
+function parseFile(filename){
+    d3.csv(filename, function (data) {
+        return data;
+    }).then( function (data) {
+    // do something with data
+        console.log(data);
+    });
+}
+
+let submitFileNameBtn = document.getElementById('submitFileName');
+submitFileNameBtn.addEventListener('click', function getFileInput() {
+    let filename = document.getElementById("fileInput").value;
+    parseFile(filename);
+}, false);
